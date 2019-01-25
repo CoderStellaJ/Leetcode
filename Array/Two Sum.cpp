33eb85634a735176d2997b482e1c0506130a7fb0
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-01-25 15:54:20 +0800

    Update Two Sum.cpp

--- Array/Two Sum.cpp
+++ Array/Two Sum.cpp
@@ -1,6 +1,12 @@
 
 /////////////////////////////////////////////////////
 /*
+Complexity: O(n)
 
+Algorithm: 
+use a hashmap to store the value as well as the index so you can keep track of both.
+Every time, you search whether complement = target - num is in the hashmap. If exists, you can the pair.
+
+Syntax:
 
 */
