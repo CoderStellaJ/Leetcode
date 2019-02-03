4fa8889efec2bafd071523bdf102247ee55a82c2
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-02-03 08:56:25 +0800

    Create 3Sum Closest.cpp

---
+++ Array/3Sum Closest.cpp
@@ -0,0 +1,49 @@
+/*
+Complexity: O(n^2)
+
+Runtime: 4 ms, faster than 100.00% of C++ online submissions for 3Sum Closest.
+Memory Usage: 766 KB, less than 87.03% of C++ online submissions for 3Sum Closest.
+
+Syntax:
+1. maximum int in C++: INT_MAX
+2. you can add more than 2 terms in a statement
+
+Test case:
+vector size < 3
+
+*/
+/////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        	int left, right;
+          int minDiff, diff;
+          int sum, result;
+          //address the corner case
+          if(nums.size() < 3) return 0;
+          sort(nums.begin(), nums.end());
+          minDiff = INT_MAX;    //syntax
+          for(int i = 0; i < nums.size(); i++) {
+              left = i+1;
+              right = nums.size()-1;
+              while(right > left) {
+                  sum = nums.at(i) + nums.at(left) + nums.at(right);    //can add more than 2 terms!
+                  diff = sum - target;
+                  if(abs(diff) < minDiff){
+                      minDiff = abs(diff);
+                      result = sum;
+                  }
+                  if(diff < 0) {
+                      left++;
+                  }else if(diff > 0){
+                      right--;
+                  }else {
+                      return target;
+                  }
+
+              }
+          }
+    return result;
+    }
+};
