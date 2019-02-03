e27d7dad359ec18398d80b5c6359e260e13b9492
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-02-03 15:12:44 +0800

    Update Remove Nth Node From End of List.cpp

--- Linked List/Remove Nth Node From End of List.cpp
+++ Linked List/Remove Nth Node From End of List.cpp
@@ -53,7 +53,8 @@ public:
 /*
 The above algorithm could be optimized to one pass. Instead of one pointer, we could use two pointers. 
 The first pointer advances the list by n+1n+1 steps from the beginning, while the second pointer starts from the beginning of the list. 
-Now, both pointers are exactly separated by nn nodes apart. We maintain this constant gap by advancing both pointers together until the first pointer arrives past the last node. 
+Now, both pointers are exactly separated by nn nodes apart. 
+We maintain this constant gap by advancing both pointers together until the first pointer arrives past the last node. 
 The second pointer will be pointing at the nnth node counting from the last. 
 We relink the next pointer of the node referenced by the second pointer to point to the node's next next node.
 */
