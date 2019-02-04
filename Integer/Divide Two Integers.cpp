3ebf9d716775d8e1f0c9fd6e285a17e8d30c67f0
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-02-04 10:01:27 +0800

    Create Divide Two Integers.cpp

---
+++ Integer/Divide Two Integers.cpp
@@ -0,0 +1,14 @@
+/*
+For division overflow, case occurs only when dividend is INT_MIN and divisor is -1 because abs(INT_MAX)<abs(INT_MIN)
+*/
+///////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        int result = dividend/divisor;
+        return result;
+    }
+};
