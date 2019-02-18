6c91af8f884ed415966337ff629e2bc0c2763982
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-02-18 17:38:28 +0800

    Create Median of Two Sorted Arrays.cpp

---
+++ Array/Median of Two Sorted Arrays.cpp
@@ -0,0 +1,17 @@
+/*
+Complexity:O(log (m+n))
+
+Algorithm:
+From the complexity requirement, the clue is to keep partitioning the 2 arrays into half(similar to binary search).
+
+Syntax:
+1. length of a vector: myvec.size()
+2. average value and sum variables may be double instead of int
+
+Corner case:
+1. both are empty arrays
+2. one of them is empty
+3. the mid index is not an int
+
+*/
+///////////////////////////////////////////////////////////////////
