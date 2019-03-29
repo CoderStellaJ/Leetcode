3e93ad826de0b14042b1d263ea4abad54185e51d
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-03-29 16:48:05 +0800

    Create Add Binary.cpp

---
+++ String/Add Binary.cpp
@@ -0,0 +1,55 @@
+/*
+Runtime: 8 ms, faster than 68.20% of C++ online submissions for Add Binary.
+Memory Usage: 9.4 MB, less than 5.57% of C++ online submissions for Add Binary.
+
+Algorithm: addition operation
+
+Note:
+
+*/
+////////////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int add = 0;
+        int lena = a.length();
+        int lenb = b.length();
+        if(a.length() < b.length()) {
+            for(int m = 0; m < lenb - lena; m++) {  //must use lena and lenb to represent original string length. lengths change in loop
+                a = "0" + a;
+            }
+        }else if(a.length() > b.length()) {
+            for(int m = 0; m < lena - lenb; m++) {
+                b = "0" + b;
+            }
+        }
+        int sum;
+        int num1, num2;
+        string ans = "";
+        for(int i = a.length()-1; i >= 0; i--) {
+            num1 = a[i] - '0';    
+            num2 = b[i] - '0';
+            sum = num1 + num2 + add;
+	          if(sum == 0) {
+		            add = 0;
+                ans = "0" + ans;    // new digit add in front
+	          }else if(sum == 1) {
+                add = 0;
+                ans = "1" + ans;
+            }else if(sum == 2) {
+                add = 1;
+                ans = "0" + ans;
+            }else if(sum == 3) {
+		            add = 1;
+                ans = "1" + ans;
+            }
+        }
+        if(add == 1) {
+            ans = "1" + ans;
+        }
+        if(ans == "") {
+            return "0";
+        }
+	  return ans;
+    }
+};
