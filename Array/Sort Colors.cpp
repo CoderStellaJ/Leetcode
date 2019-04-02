5a032ed736bf976a8e177288ccb1420d1456d274
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-04-02 17:29:19 +0800

    Update Sort Colors.cpp

--- Array/Sort Colors.cpp
+++ Array/Sort Colors.cpp
@@ -1,15 +1,44 @@
 /*
+Runtime: 4 ms, faster than 100.00% of C++ online submissions for Sort Colors.
+Memory Usage: 8.6 MB, less than 51.04% of C++ online submissions for Sort Colors.
 
-Complexity:
+Complexity: O(N)
 
 Algorithm2: 1-pass algorithm:
+use start2 to represent the position that 2 starts and end0 represents end position of 0s
+similar to quick sorting pivot positions.
 
 Syntax:
 std::swap(a,b) is a c++ built-in function for swapping.
-
 */
 //////////////////////////////////////////////////////////////////////////////
-
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int start2 = nums.size()-1;
+        int end0 = 0;
+        int i = 0;
+        while(i <= start2){
+          if(nums[i] == 0) {
+            if(i == end0) {
+              i++;
+            }else{
+              swap(nums[i], nums[end0]);
+            }
+            end0++;
+          }else if(nums[i] == 2) {
+            if(i == start2) {
+              i++; 
+            }else {
+              swap(nums[i], nums[start2]);
+            }
+            start2--;
+          }else {
+            i++;
+          }
+        }
+    }
+};
 ///////////////////////////////////////////////////////////////////////////////
 /*
 Runtime: 4 ms, faster than 100.00% of C++ online submissions for Sort Colors.
