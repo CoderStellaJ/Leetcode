f9219185b5d406e486fb92f9d903edbf5c4e7b6d
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-04-03 15:18:41 +0800

    Update Subsets.cpp

--- Array/Subsets.cpp
+++ Array/Subsets.cpp
@@ -8,5 +8,6 @@ std::vector<Foo*> f;
 std::vector<Foo*> cp = f; //deep copy (of pointers), or shallow copy (of objects).
 //All pointers to Foo are copied, but not Foo themselves
 
+vector<int> newvec = vec; //is deep copy
 */
 ////////////////////////////////////////////////////////////////////
