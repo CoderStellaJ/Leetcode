14ff493bb1d1803afae0b47bc29834413e8e426c
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-04-03 15:17:28 +0800

    Create Subsets.cpp

---
+++ Array/Subsets.cpp
@@ -0,0 +1,12 @@
+/*
+Syntax:
+1. Copy a vector
+std::vector<Foo> f;
+std::vector<Foo> cp = f; //deep copy. All Foo copied
+
+std::vector<Foo*> f;
+std::vector<Foo*> cp = f; //deep copy (of pointers), or shallow copy (of objects).
+//All pointers to Foo are copied, but not Foo themselves
+
+*/
+////////////////////////////////////////////////////////////////////
