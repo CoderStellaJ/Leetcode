136714069696217a63c858b47416d0539c7676e5
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-05-10 09:48:11 +0800

    Update Buy and Sell Stock.cpp

--- Array/Buy and Sell Stock.cpp
+++ Array/Buy and Sell Stock.cpp
@@ -1,9 +1,37 @@
 /*
+Runtime: 8 ms, faster than 98.70% of C++ online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 9.8 MB, less than 5.08% of C++ online submissions for Best Time to Buy and Sell Stock.
 
+Complexity:
+Runtime: O(2*N)
+Space: O(N)
+
+Algorithm 2:
+The first loop only creates large
+And the second loop keeps min while comparing whether it generates max profit
 */
 ////////////////////////////////////////////////////////////////////////////////////////////
-
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size();
+        if(len == 0) return 0;
+        int min = prices[0];
+        int max = prices[len-1];
+        vector<int> large(len);
+        for(int i = 0; i < len; i++) {
+          //determine the large
+            if(prices[len-1-i] > max) max = prices[len-1-i];
+            large[len-1-i] = max;
+        }
+        int maxpro = 0;
+        for(int i = 0; i < len; i++) {
+            if(prices[i] < min) min = prices[i];
+            if(large[i] - min > maxpro) maxpro = large[i] - min;
+        }
+        return maxpro;
+    }
+};
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /*
