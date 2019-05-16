635c1c0a769f93d9446834d987d81d958711696c
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-05-16 16:39:00 +0800

    Create Pow(x, n).java

---
+++ Basic Types/Pow(x, n).java
@@ -0,0 +1,12 @@
+/*
+Syntax:
+1. Format a string
+DecimalFormat df = new DecimalFormat();
+df.setMaximumFractionDigits(2);
+System.out.println(df.format(decimalNumber));
+2. Format a double
+(double)Math.round(value * 100000d) / 100000d
+3. Absolute value
+Math.abs(int a)
+*/
+//////////////////////////////////////////////////////////////////////////////
