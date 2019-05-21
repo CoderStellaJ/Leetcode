d61817c12d31e4a1be26d3682dd76388f1f6d6c3
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-05-21 22:47:22 +0800

    Create Reverse Nodes in k-Group.java

---
+++ Linked List/Reverse Nodes in k-Group.java
@@ -0,0 +1,24 @@
+/*
+Algorithm:
+
+
+Syntax:
+1. public class
+When a class is declared with the public keyword, 
+it means this class is accessible and visible to all the classes in all the packages in Java.
+
+2. instantiate a public class
+ListNode cur;  
+
+3. Pass Parameters
+The statement that "java is always pass-by-value" is technically correct, but it can be very misleading, 
+because as you have just witnessed, when you pass an object to a function, the function can modify the contents of the object, 
+so it appears that the object has been passed by reference, and not by value. So, what is happening?
+Pointers. That's what's happening.
+So, the statement "java is always pass-by-value" is true from a strictly technical, 
+but ultimately not very useful point of view, according to which objects are never really passed anywhere, 
+only pointers to the objects are passed.
+
+4. null in java not NULL
+*/
+///////////////////////////////////////////////////////////////////////////////////
