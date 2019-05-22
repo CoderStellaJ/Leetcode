706a1f25953d1e2175c7b26ce2a7c648acbe213c
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-05-22 23:07:04 +0800

    Create Basic Calculator.java

---
+++ Stack/Basic Calculator.java
@@ -0,0 +1,15 @@
+/*
+Syntax:
+1. convert character to int
+Character.getNumericValue()
+2. for number >= 10, need to use String instead of Character
+3. remove all white spaces in a string
+mystr.replaceAll(" ","");
+4. compare 2 strings
+cannot use ==, because it compares whether the 2 strings are the same object
+we can only use str1.equals(str2) actually compares the content of the string
+This method returns true if the String are equal; false otherwise.
+5. stack.pop()
+This method returns the element present at the top of the stack and then removes it.
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////
