e3e87ae12211732d50266b67950fe55ce75e444d
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-05-27 22:24:36 +0800

    Update and rename Basic Calculator.java to Basic Calculator II.java

--- Stack/Basic Calculator II.java
+++ Stack/Basic Calculator II.java
@@ -1,15 +1,28 @@
 /*
+Algorithm:
+
 Syntax:
 1. convert character to int
 Character.getNumericValue()
 2. for number >= 10, need to use String instead of Character
 3. remove all white spaces in a string
-mystr.replaceAll(" ","");
+mysre = mystr.replaceAll("\\s","");
+remember to assign the replaced result back to mystr!
 4. compare 2 strings
 cannot use ==, because it compares whether the 2 strings are the same object
 we can only use str1.equals(str2) actually compares the content of the string
 This method returns true if the String are equal; false otherwise.
 5. stack.pop()
 This method returns the element present at the top of the stack and then removes it.
+6. Character.isDigit(mychar)
+7. split a string based on multiple delimiters
+String[] operands = s.split("[+|-|*|/]");
+8. string comparison: mystr.equals("")
+9.queue
+offer
+poll
+peek
+
 */
 //////////////////////////////////////////////////////////////////////////////////////////////
+
