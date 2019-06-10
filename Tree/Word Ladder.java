1dede27c17e2e48a3784769de63c5cc692d1ce76
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-06-10 16:04:13 +0800

    Update Word Ladder.java

--- Tree/Word Ladder.java
+++ Tree/Word Ladder.java
@@ -1,4 +1,36 @@
 /*
+Complexity:
+runtime: O(MN)
+space: O(MN)
+
+Algorithm 2: BFS
+1. Do the pre-processing on the given wordList and find all the possible generic/intermediate states. 
+Save these intermediate states in a dictionary with key as the intermediate word and 
+value as the list of words which have the same intermediate word.
+
+2. Push a tuple containing the beginWord and 1 in a queue. The 1 represents the level number of a node. 
+We have to return the level of the endNode as that would represent the shortest sequence/distance from the beginWord.
+
+3. To prevent cycles, use a visited dictionary.
+
+4. While the queue has elements, get the front element of the queue. Let's call this word as current_word.
+
+5. Find all the generic transformations of the current_word and 
+find out if any of these transformations is also a transformation of other words in the word list. 
+This is achieved by checking the all_combo_dict.
+
+6. The list of words we get from all_combo_dict are all the words which have a common intermediate state with the current_word. 
+These new set of words will be the adjacent nodes/words to current_word and hence added to the queue.
+
+7. Hence, for each word in this list of intermediate words, 
+append (word, level + 1) into the queue where level is the level for the current_word.
+
+8. Eventually if you reach the desired word, its level would represent the shortest transformation sequence length.
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/*
 Time Limit Exceeded
 About the question:
 1. endWord should also be in the wordlist
