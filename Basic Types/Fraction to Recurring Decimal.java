84dfafab0a63b93e75e668c2b276677d80fc4baf
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-06-24 17:31:24 +0800

    Update Fraction to Recurring Decimal.java

--- Basic Types/Fraction to Recurring Decimal.java
+++ Basic Types/Fraction to Recurring Decimal.java
@@ -1,9 +1,14 @@
 /*
+Note:
+1. calculation type of question: use long type instead of int !
+
 Corner case:
-1. 2/1 -> 2 instead of 2.
-2. 1/6 -> 0.1(6)
-3. -50/8 -> -6.25
-4. overflow 
+1. 2/1 -> 2 instead of 2.   //take care of the . at the end
+2. 1/6 -> 0.1(6)    //rem and div relationship
+3. -50/8 -> -6.25   //sign
+4. overflow     //use long type
 -1/-2147483648
+5. 
+2147483647/37  -> "58040098.(567)" instead of "58(040098.567)"
 */
 /////////////////////////////////////////////////////////////////////
