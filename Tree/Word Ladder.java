41e07c30e0ba66f794ec015fb3a9bc3f0efb83c4
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-06-25 11:49:54 +0800

    Update Word Ladder.java

--- Tree/Word Ladder.java
+++ Tree/Word Ladder.java
@@ -1,37 +1,82 @@
 /*
+Runtime: 38 ms, faster than 87.42% of Java online submissions for Word Ladder.
+Memory Usage: 45.4 MB, less than 20.90% of Java online submissions for Word Ladder.
+
 Complexity:
-runtime: O(MN)
-space: O(MN)
+runtime: O(M*N)
+where M is the length of words and N is the total number of words in the input word list. 
+Finding out all the transformations takes M iterations for each of the N words. 
+Also, breadth first search in the worst case might go to each of the N words.
+space: O(M*N)
+ to store all M transformations for each of the N words
 
-Algorithm 2: BFS
+Algorithm: Breadth-first Search
 1. Do the pre-processing on the given wordList and find all the possible generic/intermediate states. 
-Save these intermediate states in a dictionary with key as the intermediate word and 
-value as the list of words which have the same intermediate word.
-
+Save these intermediate states in a dictionary with key as the intermediate word 
+and value as the list of words which have the same intermediate word.
 2. Push a tuple containing the beginWord and 1 in a queue. The 1 represents the level number of a node. 
 We have to return the level of the endNode as that would represent the shortest sequence/distance from the beginWord.
-
 3. To prevent cycles, use a visited dictionary.
-
 4. While the queue has elements, get the front element of the queue. Let's call this word as current_word.
-
-5. Find all the generic transformations of the current_word and 
-find out if any of these transformations is also a transformation of other words in the word list. 
+5. Find all the generic transformations of the current_word 
+and find out if any of these transformations is also a transformation of other words in the word list. 
 This is achieved by checking the all_combo_dict.
-
 6. The list of words we get from all_combo_dict are all the words which have a common intermediate state with the current_word. 
 These new set of words will be the adjacent nodes/words to current_word and hence added to the queue.
-
 7. Hence, for each word in this list of intermediate words, 
 append (word, level + 1) into the queue where level is the level for the current_word.
-
 8. Eventually if you reach the desired word, its level would represent the shortest transformation sequence length.
+
+Syntax:
+1. java pair
+import javafx.util.Pair;
+
 */
-/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+import javafx.util.Pair;
+
+class Solution {
+  public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    int len = beginWord.length();
+    Map<String, List<String>> allComboDict = new HashMap<>();
+    for(String word:wordList){
+      for(int i = 0; i < len; i++) {
+        String newword = word.substring(0,i)+"*"+word.substring(i+1,len);
+        List<String> transformations = allComboDict.getOrDefault(newword, new ArrayList<String>());
+        transformations.add(word);
+        allComboDict.put(newword, transformations);
+      }
+    }
+    Queue<Pair<String, Integer>> que = new LinkedList<>();
+    que.add(new Pair(beginWord, 1));
+    
+    Map<String, Boolean> visited = new HashMap<>();
+    visited.put(beginWord, true);
+    
+    while(!que.isEmpty()){
+      Pair<String, Integer> node = que.poll();
+      String word = node.getKey();
+      int level = node.getValue();
+      for(int i = 0; i < len; i++) {
+        String newword = word.substring(0, i)+"*"+word.substring(i+1);
+        for(String adj:allComboDict.getOrDefault(newword, new ArrayList<String>())){
+          if(adj.equals(endWord)) return level+1;
+          if(!visited.containsKey(adj)){
+            visited.put(adj, true);
+            que.add(new Pair(adj, level+1));
+          }
+        }
+      }
+    }
+    return 0;
+          
+  }
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 Time Limit Exceeded
+
 About the question:
 1. endWord should also be in the wordlist
 2. return the path length not the times of transformation. So the base case returns 2 instead of 1
