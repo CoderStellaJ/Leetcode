357e04d5afabbebe25918f432d220b7a3ef88bf7
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-06-26 11:36:15 +0800

    Create Tenth Line.java

---
+++ Bash/Tenth Line.java
@@ -0,0 +1,12 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Bash online submissions for Tenth Line.
+Memory Usage: 3.7 MB, less than 75.27% of Bash online submissions for Tenth Line.
+
+Syntax:
+tail and head
+*/
+/////////////////////////////////////////////////////////////////////
+# Read from the file file.txt and output the tenth line to stdout.
+tail -n +10 file.txt | head -1
+//tail get the 10th line onwards(including 10th line)
+//head get the first line from the tail output
