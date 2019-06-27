6370586661ed476afa446c7d46858b7f96134a83
Shuyuan <40347063+CoderStellaJ@users.noreply.github.com>
2019-06-27 11:36:27 +0800

    Create N-Queens.java

---
+++ Tree/N-Queens.java
@@ -0,0 +1,7 @@
+/*
+Background:
+The queen is the most powerful piece in the game of chess, 
+able to move any number of squares vertically, horizontally or diagonally. 
+
+*/
+////////////////////////////////////////////////////////////////////////////////
